cdfa656c03862c78ae6f94ee6cb8ea985bc2b2a6
Le_Chat_Blanc <108530916+freeman649@users.noreply.github.com>
2024-02-07 18:58:44 +0100

    Add files via upload

---
+++ CMD Bypass.bat
@@ -0,0 +1,14 @@
+@echo off
+color 4
+title CMD SECURITY BYPASS
+
+echo.
+echo ***************************
+echo *  Admin Security Bypass  *
+echo *  by le_chat_blanc999    *
+echo ***************************
+
+:a
+Set /p comm=CMD Bypass : 
+%comm%
+Goto a
